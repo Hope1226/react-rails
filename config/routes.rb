88e91d0f678bc31cca6c1fda74dee01e7c50a255
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-22 03:03:11 +0500

    Recap

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :v1, default: { format: 'json' } do
-    get 'things', to: 'things#index'
-  end
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
